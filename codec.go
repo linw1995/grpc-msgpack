deb34a6e02c343a1a1259cde8c0f54d1b5fbaff8
林玮 (Jade Lin) <linw1995@icloud.com>
2024-02-04 09:23:39 +0800

    MsgPack Encoder use array encoded structs by default

--- codec.go
+++ codec.go
@@ -8,13 +8,14 @@ import (
 )
 
 func init() {
-	codec := MsgPack{OmitEmpty: true, CustomStructTag: "json"}
+	codec := MsgPack{OmitEmpty: true, CustomStructTag: "json", UseArrayEncodedStructs: true}
 	encoding.RegisterCodec(codec)
 }
 
 type MsgPack struct {
-	OmitEmpty       bool
-	CustomStructTag string
+	OmitEmpty              bool
+	CustomStructTag        string
+	UseArrayEncodedStructs bool
 }
 
 func (_ MsgPack) Name() string {
@@ -28,6 +29,7 @@ func (j MsgPack) Marshal(v interface{}) (out []byte, err error) {
 	)
 	encoder.SetOmitEmpty(j.OmitEmpty)
 	encoder.SetCustomStructTag(j.CustomStructTag)
+	encoder.UseArrayEncodedStructs(j.UseArrayEncodedStructs)
 	if err = encoder.Encode(v); err != nil {
 		return
 	}
